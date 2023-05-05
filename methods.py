7acd5dbfb857461a1fe0dd16b96f67c732b38e9a
Helena Persichetti Paniagua <helenapersichetti@gmail.com>
2023-05-05 09:11:41 -0300

    Aula 09
    Signed-off-by: Helena Persichetti Paniagua <helenapersichetti@gmail.com>

--- methods.py
+++ methods.py
@@ -5,3 +5,20 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def soma(width, height):
+    soma = width + height
+    return soma 
+
+def subtracao(width, height):
+    subtracao = width - height
+    return subtracao
+
+def multiplicacao(width, height):
+    multiplicacao = width * height
+    return multiplicacao
+
+def divisao(width, height):
+    divisao = width/height
+    return divisao
+
